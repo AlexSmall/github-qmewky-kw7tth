9764b6129e8d27bef028af9b070008474089ee98
AlexSmall <alexander.smolyakov@gmail.com>
2021-11-19 21:56:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-qmewky-kw7tth
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-qmewky-kw7tth)
---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  cols = [
+    { field: 'vin', header: 'Vin' },
+    { field: 'year', header: 'Year' },
+    { field: 'brand', header: 'Brand' },
+    { field: 'color', header: 'Color' },
+  ];
+
+  data = [
+    { brand: 'VW', year: 2012, color: 'Orange', vin: 'dsad231ff' },
+    { brand: 'Audi', year: 2011, color: 'Black', vin: 'gwregre345' },
+    { brand: 'Renault', year: 2005, color: 'Gray', vin: 'h354htr' },
+    { brand: 'BMW', year: 2003, color: 'Blue', vin: 'j6w54qgh' },
+    { brand: 'Mercedes', year: 1995, color: 'Orange', vin: 'hrtwy34' },
+    { brand: 'Volvo', year: 2005, color: 'Black', vin: 'jejtyj' },
+    { brand: 'Honda', year: 2012, color: 'Yellow', vin: 'g43gr' },
+    { brand: 'Jaguar', year: 2013, color: 'Orange', vin: 'greg34' },
+    { brand: 'Ford', year: 2000, color: 'Black', vin: 'h54hw5' },
+    { brand: 'Fiat', year: 2013, color: 'Red', vin: '245t2s' },
+  ];
+
+  public dtSelectedRows: any[];
+
+  setSelectedItem() {
+    this.dtSelectedRows = [this.data[0]['vin']];
+  }
+
+  removeUnvalidItem() {
+    if (this.dtSelectedRows) {
+      this.dtSelectedRows = this.dtSelectedRows.filter(
+        (car) => car.year > 2005
+      );
+    }
+  }
+
+  keyFunction(rowData: any): any {
+    return rowData.vin + rowData.brand;
+  }
+}
